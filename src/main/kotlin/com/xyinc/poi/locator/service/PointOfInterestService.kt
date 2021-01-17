2df5abd8f7c8ff66ff5f747556b25646e18f5e00
Alan Nishimori <alan.nishimori@gmail.com>
2021-01-17 19:35:38 -0300

    Adding swagger to the application

--- src/main/kotlin/com/xyinc/poi/locator/service/PointOfInterestService.kt
+++ src/main/kotlin/com/xyinc/poi/locator/service/PointOfInterestService.kt
@@ -1,40 +1,15 @@
 package com.xyinc.poi.locator.service
 
-import com.xyinc.poi.locator.exception.EntityNotFoundException
 import com.xyinc.poi.locator.model.PointOfInterest
-import com.xyinc.poi.locator.repository.PointOfInterestRepository
-import org.springframework.stereotype.Service
 
-@Service
-class PointOfInterestService(
-    private val poiRepository: PointOfInterestRepository
-) {
+interface PointOfInterestService {
+    fun create(poi: PointOfInterest): PointOfInterest
 
-    fun create(poi: PointOfInterest): PointOfInterest {
-        validatePosition(poi.posX.toDouble(), poi.posY.toDouble())
+    fun getPointOfInterestById(id: String): PointOfInterest
 
-        return poiRepository.save(poi)
-    }
+    fun getPointOfInterestByName(name: String): List<PointOfInterest>
 
-    fun getPointOfInterestById(id: String): PointOfInterest = poiRepository.findById(id) ?:
-        throw EntityNotFoundException("No point of interest found for id=$id")
+    fun getAll(): List<PointOfInterest>
 
-    fun getPointOfInterestByName(name: String): List<PointOfInterest> = poiRepository.findByName(name)
-
-    fun getAll(): List<PointOfInterest> = poiRepository.findAll()
-
-    fun findByDistance(posX: Double, posY: Double, maxDistance: Double): List<PointOfInterest> {
-        validatePosition(posX, posY)
-
-        return poiRepository.findByLocation(
-            posX = posX,
-            posY = posY,
-            maxDistance = maxDistance
-        )
-    }
-
-    private fun validatePosition(posX: Double, posY: Double) {
-        if (posX < 0 || posY < 0)
-            throw IllegalArgumentException("posX and posY must be positive")
-    }
+    fun findByDistance(posX: Double, posY: Double, maxDistance: Double): List<PointOfInterest>
 }
